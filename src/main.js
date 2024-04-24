678072494b2be7eda9519e1e1fb37f18afaaba50
rbrickmn <rbrickmn@gmail.com>
2024-04-24 12:07:52 -0400

    Add title & instructions section

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/warning.css";
+import "../styles/components/title.css";
+import "../styles/components/instructions.css";
 import "../styles/utils.css";
